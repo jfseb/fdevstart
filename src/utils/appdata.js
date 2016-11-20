429f10ad72340877a8091a6df271eb47fd8b52c2
jfseb <jfsebpublic@gmail.com>
2016-11-20 22:55:45 +0100

    Model and appdata with persistence

--- src/utils/appdata.js
+++ src/utils/appdata.js
@@ -1,4 +1,4 @@
-(function () {
+;(function () {
   'use strict'
   var process = require('process')
   var dbg = require('debug')
@@ -38,5 +38,76 @@
   AppData.prototype.getProperty = function (sKey, oValue) {
     return this.data[sKey]
   }
-  module.exports = AppData
+
+  function PersistenceHandle (sAppName, sFileName) {
+    this.AppName = sAppName
+    logger.log('Dest' + sAppName)
+    this.dir = getUserHome() + path.sep + sAppName
+    this.fileName = this.dir + path.sep + sFileName
+    this.filename = process.env['USERDATA']
+    this.data = {}
+    if (!fs.existsSync(this.dir)) {
+      fs.mkdirSync(this.dir)
+    }
+    try {
+      this.data = JSON.parse(fs.readFileSync(this.fileName))
+    } catch (e) {
+      this.data = {}
+    }
+  }
+
+  PersistenceHandle.prototype.writeFile = function (file, data, cb) {
+    var sData = JSON.stringify(data)
+    fs.writeFile(this.fileName, sData, {
+      // encoding : "utf8",
+      flag: 'w'
+    }, (err) => {
+      if (err) {
+        cb(err)
+      }
+      cb()
+    })
+  }
+
+  PersistenceHandle.prototype.save = function (data, cb) {
+    var that = this
+    fs.stat(this.dir, function (err, stat) {
+      if (err == null) {
+        that.writeFile(that.fileName, data, cb)
+      } else if (err.code === 'ENOENT') {
+        // file does not exist
+        fs.mkdir(this.dir, (err) => {
+          if (err) {
+            cb(err)
+          }
+          that.writeFile(that.fileName, data, cb)
+        }
+        )
+      } else {
+        console.log('Some other error: ', err.code)
+        cb(err)
+      }
+    })
+  }
+
+  PersistenceHandle.prototype.load = function (cb) {
+    fs.readFile(this.fileName, 'utf8', function (err, data) {
+      if (err) {
+        cb(err)
+        return
+      }
+      try {
+        var sData = JSON.parse(data)
+      } catch (e) {
+        cb(e)
+        return
+      }
+      cb(undefined, sData)
+    })
+  }
+
+  module.exports = {
+    AppData: AppData,
+    PersistenceHandle: PersistenceHandle
+  }
 }())
--- test/utils/appdata.nunit.js
+++ test/utils/appdata.nunit.js
@@ -1,23 +1,42 @@
-
 var AppData = require('../../src/utils/appdata.js')
 
 exports.testCtor = function (test) {
-    // test.expect(3);
+  // test.expect(3)
   var u = new AppData('_Test1', 'File1')
   test.deepEqual(typeof u, 'object', 'is object')
   test.done()
 }
 
 exports.testCtor = function (test) {
-    // test.expect(3);
+  // test.expect(3)
   var u
   var k
-  u = new AppData('_Test2', 'File1')
+  u = new AppData.AppData('_Test2', 'File1')
   u.setProperty('key1', {
     a: 42
   })
 
-  k = new AppData('_Test2', 'File1')
+  k = new AppData.AppData('_Test2', 'File1')
   test.deepEqual(k.getProperty('key1').a, 42)
   test.done()
+}
+
+exports.testPersistence = function (test) {
+  test.expect(2)
+  var u
+  u = new AppData.PersistenceHandle('_Test3', 'File1')
+  u.save({
+    a: 1
+  }, function (err) {
+    if (err) {
+      console.log('got an error' + err)
+      throw err
+    }
+    var k = new AppData.PersistenceHandle('_Test3', 'File1')
+    k.load(function (err, sData) {
+      test.equal(err, undefined, 'no error')
+      test.deepEqual(sData, {a: 1}, 'correct data read')
+      test.done()
+    })
+  })
 }
